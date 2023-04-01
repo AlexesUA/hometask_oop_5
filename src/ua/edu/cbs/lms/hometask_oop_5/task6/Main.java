ed035894d7d5bc7cd2fecb9d5d342aef5110becd
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-01 17:20:11 +0300

    Created list teachers

--- src/ua/edu/cbs/lms/hometask_oop_5/task6/Main.java
+++ src/ua/edu/cbs/lms/hometask_oop_5/task6/Main.java
@@ -1,7 +1,25 @@
 package ua.edu.cbs.lms.hometask_oop_5.task6;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        List<String> nameTeachers = new ArrayList<>();
+
+        nameTeachers.add("Ковальчук Олексій Віталійович");
+        nameTeachers.add("Шевченко Юлія Олегівна");
+        nameTeachers.add("Бойко Сергій Володимирович");
+        nameTeachers.add("Петренко Оксана Василівна");
+        nameTeachers.add("Лисенко Вікторія Ігорівна");
+        nameTeachers.add("Мельник Олександра Сергіївна");
+        nameTeachers.add("Григоренко Ігор Михайлович");
+        nameTeachers.add("Кравченко Наталія Олексіївна");
+        nameTeachers.add("Бондаренко Андрій Вікторович");
+        nameTeachers.add("Іваненко Марина Петрівна");
+
+
 
     }
 }
