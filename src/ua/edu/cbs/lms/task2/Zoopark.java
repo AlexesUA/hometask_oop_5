39d2ca6fa3fedf04d882b4a9d49e7379af384910
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-01 13:07:51 +0300

    Create Zoopark class

--- src/ua/edu/cbs/lms/task2/Zoopark.java
+++ src/ua/edu/cbs/lms/task2/Zoopark.java
@@ -1,5 +1,32 @@
 package ua.edu.cbs.lms.task2;
 
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class Zoopark {
-    //List<>
+    private List<Animal> animalList;
+
+    public Zoopark(){
+        animalList = new ArrayList<>();
+    }
+    public void addAnimal(Animal animal){
+        animalList.add(animal);
+    }
+
+    public void addAnimal(int index, Animal animal){
+        animalList.add(index, animal);
+    }
+
+    public void showZoopark(){
+        int index = 0;
+        for (Animal animal:animalList) {
+            System.out.println((index+1) + animal.toString());
+            index++;
+        }
+    }
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
