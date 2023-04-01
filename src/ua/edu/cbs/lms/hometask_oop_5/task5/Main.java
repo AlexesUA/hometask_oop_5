cd242772fc4b7d661651c1a3df1197a2608ecc92
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-01 16:58:22 +0300

    Finished task

--- src/ua/edu/cbs/lms/hometask_oop_5/task5/Main.java
+++ src/ua/edu/cbs/lms/hometask_oop_5/task5/Main.java
@@ -3,5 +3,26 @@ package ua.edu.cbs.lms.hometask_oop_5.task5;
 public class Main {
     public static void main(String[] args) {
 
+        Friends friends = new Friends();
+        friends.addFriend("Наталя");
+        friends.addFriend("Ігор");
+        friends.addFriend("Ольга");
+        friends.addFriend("Володимир");
+        friends.addFriend("Юлія");
+        friends.addFriend("Василь");
+        friends.addFriend("Тетяна");
+        friends.addFriend("Олексій");
+        friends.addFriend("Юрій");
+        friends.addFriend("Людмила");
+
+
+        System.out.println("Before sorting");
+        friends.showFriends();
+
+
+        System.out.println("\n\nAfter sorting");
+        friends.sort();
+        friends.showFriends();
+
     }
 }
