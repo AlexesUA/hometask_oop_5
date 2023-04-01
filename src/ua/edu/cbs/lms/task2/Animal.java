1dcdfc87ae905fa2ffb0280e9038b17e19f08330
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-01 12:00:50 +0300

    Create Lion class

--- src/ua/edu/cbs/lms/task2/Animal.java
+++ src/ua/edu/cbs/lms/task2/Animal.java
@@ -18,28 +18,38 @@ public abstract class Animal implements ICharacteristic, IDemeanor{
         this.isCarnivores = isCarnivores;
     }
 
+//    public static Animal addAnimal(int age, String covered, String limbs, String food, String move, boolean isCarnivores){
+//        if(age<0){
+//            System.out.println("Вік не може бути менше 0.");
+//            return null;
+//        }
+//        return new Animal(age, covered, limbs, food, move, isCarnivores);
+//    }
+
+
     @Override
     public String covered() {
-        return null;
+        return covered;
     }
 
     @Override
     public String limbs() {
-        return null;
+        return limbs;
     }
 
     @Override
     public String food() {
-        return null;
+        return food;
     }
 
     @Override
     public String move() {
-        return null;
+        return move;
     }
 
     @Override
     public String aggression() {
-        return null;
+        if(isCarnivores) return "Хижак";
+        else return "Травоядне";
     }
 }
---
+++ src/ua/edu/cbs/lms/task2/Lion.java
@@ -0,0 +1,18 @@
+package ua.edu.cbs.lms.task2;
+
+public class Lion extends Animal{
+
+    private Lion(int age){
+        super(age, "Хутро", "4 лапи", "М'ясо", "Бігає", true);
+    }
+
+    public static Lion addLion(int age){
+        if(age<0){
+            System.out.println("Вік не може бути менше 0.");
+            return null;
+        }
+
+        return new Lion(age);
+    }
+
+}
